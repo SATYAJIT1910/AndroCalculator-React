1be30d2750df6a2d07aa561462adce83a051cb5c
Satyajit Ghosh <satyajit.edu@outlook.com>
2021-03-20 09:26:57 +0530

    heading added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css"
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:"#383CC1",color:"white",padding:"2px",fontFamily:"sans-serif"}}>
+
+      <h1 style={{justifyContent:"center",display:"flex"}}>AndroCalculator</h1>
+      <h2 style={{justifyContent:"center",display:"flex"}}>Inspired from Google Calculator</h2>
+      <h3 style={{justifyContent:"center",display:"flex"}}>Made by Satyajit Ghosh</h3>
+
     </div>
   );
-}
 
+
+}
 export default App;

433effe47e246e60109ff4bbe2c0c0732f492a80
Satyajit Ghosh <satyajit.edu@outlook.com>
2021-03-20 10:29:53 +0530

    scientific Buttons are added .

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -5,7 +5,29 @@ function Calculator(){
     return(
         <div style={{display:"grid",justifyContent:"center"}}>
             <div style={{padding:"20px"}}/>
-            <div style={{background:"#1a74e9"}}>
+            <div style={{background:"black"}}>
+            <div>
+                <button className="buttonsup">INV</button>
+                <button className="buttonsup">RAD</button>
+                <button className="buttonsup">sin</button>
+                <button className="buttonsup">cos</button>
+                <button className="buttonsup">tan</button>
+            </div>
+            <div>
+                <button className="buttonsup">%</button>
+                <button className="buttonsup">ln</button>
+                <button className="buttonsup">log</button>
+                <button className="buttonsup">√</button>
+                <button className="buttonsup">^</button>
+            </div>
+            <div>
+                <button className="buttonsup">∏</button>
+                <button className="buttonsup">е</button>
+                <button className="buttonsup">(</button>
+                <button className="buttonsup">)</button>
+                <button className="buttonsup">!</button>
+            </div>
+            
             <div>
                 <button className="buttons">7</button>
                 <button className="buttons">8</button>
@@ -13,13 +35,13 @@ function Calculator(){
                 <button className="buttons">÷</button>
                 <button className="buttons">⌫</button>
             </div>
-            <div style={{background:"#1a74e9"}}>
+            <div style={{background:"black"}}>
                 <button className="buttons">4</button>
                 <button className="buttons">5</button>
                 <button className="buttons">6</button>
                 <button className="buttons">x</button>
             </div>
-            <div  style={{background:"#1a74e9"}}>
+            <div  style={{background:"black"}}>
                 <button className="buttons">1</button>
                 <button className="buttons">2</button>
                 <button className="buttons">3</button>
